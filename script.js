ed9e7786b1c1838f9ad345e3373366e57329c69d
= <=>
2022-08-11 23:27:19 +0300

    Make buttons visible

--- script.js
+++ script.js
@@ -5,15 +5,20 @@ const buttons = document.createElement("div");
 buttons.innerHTML = `<button class="buttons">Easy</button>
                      <button class="buttons">Medium</button>
                      <button class="buttons">Hard</button`;
-style.innerHTML +=  `.buttons-div {
-                        display: flex;
-                        flex-direction: column;
-                        gap: 10px;
-                        max-width: 100px;
-                        z-index: 10;
-                    }
-                    .buttons {
-                        padding: 5px 10px;
-                    }`
+style.innerHTML +=  `
+.buttons - div {
+    display: flex;
+    position: absolute;
+    top: 0px;
+    flex-direction: column;
+    gap: 10px;
+    max-width: 100px;
+    z-index: 100;
+    background-color: #E0E0E0;
+}
+.buttons {
+    padding: 5px 10px;
+} `
 
-
+buttons.classList.toggle("buttons-div");
+body.appendChild(buttons);
