4f74ccfe5975530a9418a3eecf9e7e079501949c
= <=>
2022-08-11 23:05:23 +0300

    add classes to elements

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const style = document.querySelector("style");
 const body = document.querySelector("body");
 const buttons = document.createElement("div");
 
-buttons.innerHTML = `<button>Easy</button>
-                     <button>Medium</button>
-                     <button>Hard</button`;
-style.innerHTML = `.buttons-div {
+buttons.innerHTML = `<button class="buttons">Easy</button>
+                     <button class="buttons">Medium</button>
+                     <button class="buttons">Hard</button`;
+style.innerHTML +=  `.buttons-div {
                         display: flex;
                         flex-direction: column;
                         gap: 10px;
@@ -15,3 +15,5 @@ style.innerHTML = `.buttons-div {
                     .buttons {
                         padding: 5px 10px;
                     }`
+
+
